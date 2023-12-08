8d9ca801a432acd72c3f237090a78ee32bc0e3ed
IagoGondim <iago-gondim@gotmail.com>
2023-12-08 11:23:09 -0300

    first commit

---
+++ src/main/java/com/iagogondim/atividaderestapi/AtividaderestapiApplication.java
@@ -0,0 +1,13 @@
+package com.iagogondim.atividaderestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividaderestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividaderestapiApplication.class, args);
+	}
+
+}
