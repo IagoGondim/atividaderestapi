27701251019a5aa84b399da5ab36af5180edce22
IagoGondim <iago-gondim@gotmail.com>
2023-12-08 11:24:52 -0300

    add application.properties com as propriedades do banco de teste H2

--- src/main/java/com/iagogondim/atividaderestapi/AtividaderestapiApplication.java
+++ src/main/java/com/iagogondim/atividaderestapi/AtividaderestapiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AtividaderestapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AtividaderestapiApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(AtividaderestapiApplication.class, args);
+  }
 
 }
